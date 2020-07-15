ede327bb1306628342bd72eb489a299b061fa184
Sanych369 <61685572+Sanych369@users.noreply.github.com>
2020-07-15 17:19:28 +0600

    Create 6.3.java

---
+++ 6/6.3.java
@@ -0,0 +1,56 @@
+/*
+
+Реализуйте generic-класс Pair, похожий на Optional, но содержащий пару элементов разных типов и не запрещающий элементам принимать значение null.
+
+Реализуйте методы getFirst(), getSecond(), equals() и hashCode(), а также статический фабричный метод of(). Конструктор должен быть закрытым (private).
+
+С корректно реализованным классом Pair должен компилироваться и успешно работать следующий код:
+Pair<Integer, String> pair = Pair.of(1, "hello");
+Integer i = pair.getFirst(); // 1
+String s = pair.getSecond(); // "hello"
+Pair<Integer, String> pair2 = Pair.of(1, "hello");
+boolean mustBeTrue = pair.equals(pair2); // true! 
+boolean mustAlsoBeTrue = pair.hashCode() == pair2.hashCode(); // true!
+
+Пожалуйста, не меняйте модификатор доступа класса Pair. Для корректной проверки класс должен иметь пакетную видимость.
+
+*/
+
+public static class Pair<T1, T2> {
+    private final T1 first;
+    private final T2 second;
+
+    private Pair(T1 first, T2 second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T1 getFirst() {
+        return first;
+    }
+
+    public T2 getSecond() {
+        return second;
+    }
+
+    public static <T1, T2> Pair<T1, T2> of(T1 first, T2 second) {
+        return new Pair<>(first, second);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(getFirst(), pair.getFirst()) &&
+                Objects.equals(getSecond(), pair.getSecond());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result = 31 * result + ((first == null) ? 0 : first.hashCode());
+        result = 31 * result + ((second == null) ? 0 : second.hashCode());
+        return result;
+    }
+}
