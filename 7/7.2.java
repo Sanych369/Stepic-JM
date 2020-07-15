90fdb4d6d93f71dfa1faec4feb42b6bad50651f2
Sanych369 <61685572+Sanych369@users.noreply.github.com>
2020-07-15 17:26:32 +0600

    Create 7.2.java

---
+++ 7/7.2.java
@@ -0,0 +1,22 @@
+/*
+
+Создайте функциональный интерфейс NumberGenerator, параметризованный таким образом, что принимает только наследников класса Number, имеющий метод boolean cond(T arg).
+
+Создайте в методе public static NumberGenerator<? super Number> getGenerator() с помощью лямбда выражения реализацию NumberGenerator, которая возвращает true, если число в int эквиваленте больше 0.
+
+Требования:
+1. Должен быть метод public static NumberGenerator<? super Number> getGenerator()
+2. Соблюдайте код стайл при параметризации.
+3. Имя параметра должно быть "Т"
+3. Метод должен возвращать корректную реализацию
+
+*/
+
+@FunctionalInterface
+public interface NumberGenerator<T extends Number> { 
+    boolean cond (T value);
+}
+
+public static NumberGenerator<? super Number> getGenerator() {
+    return x -> x.intValue() > 0;
+}
